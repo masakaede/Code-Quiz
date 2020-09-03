878c84ed278e898c2e184e3210b54ed4bb8e69e7
masakaede <pichengchang@gmail.com>
2020-09-04 00:06:29 +1000

    update html js css readme files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Code-Quiz
+
+https://masakaede.github.io/Code-Quiz/
+
+1. Simply press the Start Quiz button to start the quiz.
+<img src="img/Code-Quiz-1.png" alt="Start Page">
+
+2. Answer Five questions within 75 seconds.
+<img src="img/Code-Quiz-2.png" alt="Quiz 1">
+<img src="img/Code-Quiz-3.png" alt="Quiz 2">
+<img src="img/Code-Quiz-4.png" alt="Quiz 3">
+<img src="img/Code-Quiz-5.png" alt="Quiz 4">
+<img src="img/Code-Quiz-6.png" alt="Quiz 5">
+
+3. Enter your inital and press the Submit button.
+<img src="img/Code-Quiz-7.png" alt="All Done Page">
+
+4. Score will be recorded, to erase the record, simply press the Clear Highscores button. Or click Go Back button to restart the quiz
+<img src="img/Code-Quiz-8.png" alt="Highscores">
--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-
-//show highscore page once view highscore is pressed and hide all other pages
-//go back to start page 
-//user press button
-
-//compare answer and display message
-//change score and change time if answer is wrong
-//hide current question and show next question
-//enter initial at all done page
-//submit form and show highscore page, hide all done page ex.4-3-26
-//clear highscore and 
-
-
 var startPageEl = document.querySelector(".start-page");
 var qPageEl = document.querySelectorAll(".question-page");
 var qOnePageEl = document.querySelector(".q-1-page");
@@ -22,47 +9,40 @@ var allDonePageEl = document.querySelector(".all-done-page");
 var highscoresPageEl = document.querySelector(".highscores-page");
 var responseEl = document.querySelector(".response");
 var finalScoreEl = document.querySelector(".final-score");
-var highscoresFormEl = document.querySelector("#highscores-form");
+var highscoreEl = document.querySelector(".high-score");
 var highscoresListEl = document.querySelector("#highscores-list");
 var submitEl = document.querySelector("#submit");
 var initialEl = document.querySelector("#initial");
 var goBackEl = document.querySelector(".go-back");
 var clearButtonEl = document.querySelector(".clear-button");
-
 var timeEl = document.querySelector(".time");
 var startQuizEl = document.querySelector(".start-quiz");
 
-
-
+var secondLeft;
+var answer = "";
+var timerInterval;
 
 //set timer
-var secondLeft = 75;
-var answer = "";
-
 function setTime() {
-    var timerInterval = setInterval(function () {
+    secondLeft = 75;
+    timerInterval = setInterval(function () {
         secondLeft--;
         timeEl.textContent = "Time : " + secondLeft;
 
-        //-10 when wrong answer
-
-
         if (secondLeft <= 0) {
             clearInterval(timerInterval);
-            //jump to all done page
         }
     }, 1000)
 }
 
+// button press
 for (var i = 0; i < 5; i++) {
     qPageEl[i].addEventListener("click", function (event) {
-        if (event.target.matches("button")) {
-            console.log("click")
-        };
+        var element = event.target;
 
-        if (event.target.matches("button") && event.target.matches(".correctAnswer")) {
+        if (element.matches("button") && element.matches(".correctAnswer")) {
             answer = "Correct!"
-        } else if (event.target.matches("button") && event.target.matches(".wrongAnswer")) {
+        } else if (element.matches("button") && element.matches(".wrongAnswer")) {
             answer = "Wrong!"
         };
 
@@ -72,116 +52,62 @@ for (var i = 0; i < 5; i++) {
             secondLeft = 0
         }
 
-        console.log(answer)
         responseEl.textContent = answer;
 
-        if (event.target.matches("button") && event.target.matches(".final-q")) {
-            console.log("over")
-            finalScoreEl.textContent = "You final score is " + secondLeft + " !"
-
+        if (element.matches("button") && element.matches(".q-1")) {
+            qOnePageEl.setAttribute("style", "display : none");
+            qTwoPageEl.removeAttribute("style", "display : none");
+        } else if (element.matches("button") && element.matches(".q-2")) {
+            qTwoPageEl.setAttribute("style", "display : none");
+            qThreePageEl.removeAttribute("style", "display : none");
+        } else if (element.matches("button") && element.matches(".q-3")) {
+            qThreePageEl.setAttribute("style", "display : none");
+            qFourPageEl.removeAttribute("style", "display : none");
+        } else if (element.matches("button") && element.matches(".q-4")) {
+            qFourPageEl.setAttribute("style", "display : none");
+            qFivePageEl.removeAttribute("style", "display : none");
+        } else if (element.matches("button") && element.matches(".q-5")) {
+            clearInterval(timerInterval)
+            timeEl.setAttribute("style", "display : none");
+            qFivePageEl.setAttribute("style", "display : none");
+            allDonePageEl.removeAttribute("style", "display : none");
+            finalScoreEl.textContent = "You final score is " + secondLeft + " !";
+            responseEl.textContent = ""
         }
     })
-
 };
 
+
+//run quiz
+startQuizEl.addEventListener("click", function (event) {
+    event.preventDefault();
+    startPageEl.setAttribute("style", "display : none");
+    qOnePageEl.removeAttribute("style", "display : none");
+    setTime()
+});
+
+//submit score
 submitEl.addEventListener("click", function (event) {
     event.preventDefault();
     var highscorer = "1. " + initialEl.value + " - " + secondLeft;
     var li = document.createElement("li");
     li.textContent = highscorer;
     highscoresListEl.appendChild(li);
-})
-
-clearButtonEl.addEventListener("click", function () {
-    highscoresListEl.innerHTML = "";
-})
-//loop through questions
-
-
-
-
-
-//run settime after button clicked
-startQuizEl.addEventListener("click", function (event) {
-    event.preventDefault();
-    setTime()
+    allDonePageEl.setAttribute("style", "display : none");
+    highscoresPageEl.removeAttribute("style", "display : none");
+    timeEl.textContent = ""
 });
 
+//clear button
+clearButtonEl.addEventListener("click", function (event) {
+    event.preventDefault();
+    highscoresListEl.innerHTML = "";
+});
 
-
-//
-//var questionEl = document.querySelector(".question");
-/*var answersEl = document.querySelector(".answers");
-var answer-1El = document.querySelector(".answer-1");
-var answer-2El = document.querySelector(".answer-2");
-var answer-3El = document.querySelector(".answer-3");
-var answer-4El = document.querySelector(".answer-4");
-
-
-
-
-
-var score = 0;
-
-var q1 = {
-    ques: "What are people who write computer code called?",
-    a1: "1. Programmers",
-    a2: "2. Cryptographers",
-    a3: "3. Professors",
-    a4: "4. Manufacturers",
-    correctAnswer: "1. Programmers"
-}
-
-var q2 = {
-    ques: "Which of the following deos NOT run using a computer program?",
-    a1: "1. Bicycle",
-    a2: "2. Train",
-    a3: "3. Rocket",
-    a4: "4. Car",
-    correctAnswer: "1. Bicycle"
-}
-
-var q3 = {
-    ques: "Which of the following is NOT a programming language?",
-    a1: "1. Java",
-    a2: "2. Python",
-    a3: "3. Banana",
-    a4: "4. Ruby",
-    correctAnswer: "3. Banana"
-}
-
-var q4 = {
-    ques: "Which if the following is a programming language?",
-    a1: "1. Bite",
-    a2: "2. Gnaw",
-    a3: "3. Itch",
-    a4: "4. Scratch",
-    correctAnswer: "4. Scratch"
-}
-
-var q5 = {
-    ques: "What is computer coding?",
-    a1: "1. A TV show",
-    a2: "2. Telling a computer what to do",
-    a3: "3. A radio show",
-    a4: "4. A list of function",
-    correctAnswer: "2. Telling a computer what to do"
-}
-
-//function to check if the answer is correct
-function buttonClick() {
-
-}
-
-//function to change to the next question
-
-
-
-
-
-// click button event
-button.addeventlistenser("click", function (
-
-))*/
-
-//event.preventDefault()
+//go back button
+goBackEl.addEventListener("click", function (event) {
+    event.preventDefault();
+    highscoresPageEl.setAttribute("style", "display : none");
+    startPageEl.removeAttribute("style", "display : none");
+    timeEl.removeAttribute("style", "display : none");
+});
